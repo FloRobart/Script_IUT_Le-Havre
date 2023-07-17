c570abe13c3c0bbab769cb27b85a8ff4c0c44545
Floris <florisrobart.pro@gmail.com>
2023-07-17 22:37:41 +0200

    test

--- Arborescence_IUT.sh
+++ Arborescence_IUT.sh
@@ -42,17 +42,21 @@ function arborescenceEchoue()
 function cheminExisteDeja()
 {
 	whiptail --title "Arborescence déjà existante" --yesno "Le dossier \"${1}/IUT\" existe déjà. Voullez vous le supprimez ainsi que toute les données qu'il contient ?" 10 50 && {
-        whiptail --title "CONFIRMATION" --yesno "Êtes vous sûr de vouloir supprimer votre Arborescence IUT existante ?" --yes-button "no" --no-button "yes" 10 50 && {
-            rm -rf "${1}/IUT" && {
-                whiptail --title "SUPPRESSION" --infobox "L'arborescence à été supprimé avec succès" 10 50
-                creationArborescence "$1" && arborescenceCree || arborescenceEchoue
-            };
-        } || {
-            whiptail --title "ANNULATION" --infobox "L'arborescence n'a pas été installé" 10 50;
-        };
+        echo oui
     } || {
-        whiptail --title "ANNULATION" --infobox "L'arborescence n'a pas été installé" 10 50;
+        echo non
     }
+    
+    #{
+    #    whiptail --title "CONFIRMATION" --yesno "Êtes vous sûr de vouloir supprimer votre Arborescence IUT existante ?" --yes-button "no" --no-button "yes" 10 50 && {
+    #        rm -rf "${1}/IUT" && {
+    #            whiptail --title "SUPPRESSION" --infobox "L'arborescence à été supprimé avec succès" 10 50
+    #            creationArborescence "$1" && arborescenceCree || arborescenceEchoue
+    #        };
+    #    } || {
+    #        whiptail --title "ANNULATION" --infobox "L'arborescence n'a pas été installé" 10 50;
+    #    };
+    #} || { whiptail --title "ANNULATION" --infobox "L'arborescence n'a pas été installé" 10 50; }
 
     return 0
 	
